1be8a6bf1a622161a75ca274b9817051a893b4e8
Alexander G. Zimmerman <alex.g.zimmerman@gmail.com>
2023-01-07 16:05:44 +0100

    Plot without Seaborn

--- pybud/plots.py
+++ pybud/plots.py
@@ -1,27 +1,26 @@
 from pandas import DataFrame
-from seaborn import lineplot
+from matplotlib.pyplot import axes
 
 
 def plot_balance_over_time(balance_time_history: DataFrame):
 
-    axes = lineplot(data=balance_time_history[['expected_balance', 'minimum_balance', 'maximum_balance']])
+    color = "steelblue"
+
+    ax = axes()
+
+    ax.plot(balance_time_history.date, balance_time_history.minimum_balance, "--", color=color, linewidth=1, label="Min")
+
+    ax.plot(balance_time_history.date, balance_time_history.expected_balance, "-", color=color, linewidth=2, label="Nominal")
+
+    ax.plot(balance_time_history.date, balance_time_history.maximum_balance, "--", color=color, linewidth=1, label="Max")
 
     first_date = balance_time_history.date.iloc[0]
     last_date = balance_time_history.date.iloc[-1]
-    # axes.plot((first_date, last_date), (0., 0.), "-r", linewidth=1, label="Zero")
+    ax.plot((first_date, last_date), (0., 0.), "-r", linewidth=1, label="Zero")
 
-    axes.grid(True)
+    ax.grid(True)
 
-    axes.get_legend().remove()
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Balance')
 
-    axes.set_xlabel('Date')
-    axes.set_ylabel('Balance')
-
-    for line in axes.lines:
-        line.set_color('steelblue')
-
-    axes.lines[0].set_linestyle("-")
-    axes.lines[1].set_linestyle("--")
-    axes.lines[2].set_linestyle("--")
-
-    return axes
+    return ax
